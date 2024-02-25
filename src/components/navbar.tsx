02aeed7edadee02cdf798904069aa410ddc8ff32
yequan99 <ytan213@e.ntu.edu.sg>
2024-02-25 23:21:23 +0800

    fix: Added router link to sidebar and navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,8 @@
+import Link from 'next/link';
 export default function NavBar() {
     return (
-        <div className="h-full flex items-center border-b-2 border-slate-200 pl-10">
-            <h1>
-                NTUMODS
-            </h1>
+        <div className="h-full flex items-center border-b-2 border-slate-200 pl-10 hover:cursor-pointer">
+            <Link href="/">NTUMODS</Link>
         </div>
     )
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,25 +1,28 @@
+import Link from 'next/link';
 import { timetable, course } from "@/icons/icons"
 
 export default function SideBar() {
+
+    const tabs = [
+        {name: "Timetable", icon: timetable, link:"/timetable"},
+        {name: "Courses", icon: course, link:"/courses"}
+    ]
+
     return (
         <div className="h-full flex justify-center border-r border-slate-200">
             <ul className="py-4">
-                <li className="py-4">
-                    <div className="flex flex-row items-center">
-                        <div className="w-8 h-8">
-                            {timetable}
-                        </div>
-                        <h1 className="pl-2">Timetable</h1>
-                    </div>
-                </li>
-                <li className="py-4">
-                    <div className="flex flex-row items-center">
-                        <div className="w-8 h-8">
-                            {course}
-                        </div>
-                        <h1 className="pl-2">Courses</h1>
-                    </div>
-                </li>
+                {tabs.map((item,index) => (
+                    <Link key={index} href={item.link}>
+                        <li className="py-4 hover:cursor-pointer">
+                            <div className="flex flex-row items-center">
+                                <div className="w-8 h-8">
+                                    {item.icon}
+                                </div>
+                                <h1 className="pl-2">{item.name}</h1>
+                            </div>
+                        </li>
+                    </Link>
+                ))}
             </ul>
         </div>
     )
