0211d82f30f00e89e7638fc4e98fbaf64e5c1b75
yequan99 <ytan213@e.ntu.edu.sg>
2024-04-09 00:55:23 +0800

    Retrieve current semester week info

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+
+import { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { timetable, course, search } from "@/utils/icons"
+import { timetable, course } from "@/utils/icons"
 
 export default function NavBar() {
     const pathname = usePathname().substring(1)
@@ -11,6 +13,79 @@ export default function NavBar() {
         { name: "Timetable", icon: timetable, link:"/timetable" },
         { name: "Modules", icon: course, link:"/modules" }
     ]
+
+    const getSemesterInfo = (currentDate: Date) => {
+        // const currentDate: Date = new Date()
+        const currentYear = currentDate.getFullYear() % 100
+        const currentMonth = currentDate.getMonth() + 1
+
+        if (currentMonth < 8) {
+            // sem 2
+            const january = 0
+            const firstDayOfJan = new Date(currentYear, january, 1)
+            const dayOfWeekOfFirstJan = firstDayOfJan.getDay()
+
+            let offset = 0
+            if (dayOfWeekOfFirstJan < 1) {
+                offset = 1 - dayOfWeekOfFirstJan // If the first day of January is Monday or Sunday, the second Monday is on the 8th
+            } else {
+                offset = 8 - dayOfWeekOfFirstJan // If the first day of January is any other day, the second Monday is on the 15th
+            }
+            const semStartDate = new Date(new Date().getFullYear(), january, offset + 7)
+            let weeks = 1
+            let pointer: Date = semStartDate
+
+            while (semStartDate < currentDate) {
+                weeks += 1
+                pointer.setDate(pointer.getDate() + 7)
+            }
+            weeks -= 1
+            let currentWeek = ""
+            if (weeks <= 7) {
+                currentWeek = `Week ${weeks}`
+            } else if (weeks > 13){
+                currentWeek = "Reading/Exam Week"
+            } else if (weeks > 8) {
+                currentWeek = `Week ${weeks-1}`
+            } else {
+                currentWeek = "Recess Week"
+            }
+
+            return `AY${currentYear-1}/${currentYear}, Semester 2, ${currentWeek}`
+        } else {
+            // sem 1
+            const august = 7
+            const firstDayOfAug = new Date(currentDate.getFullYear(), august, 1)
+            const dayOfWeekOfFirstAug = firstDayOfAug.getDay()
+
+            let offset = 0
+            if (dayOfWeekOfFirstAug < 1) {
+                offset = 1 - dayOfWeekOfFirstAug // If the first day of January is Monday or Sunday, the second Monday is on the 8th
+            } else {
+                offset = 8 - dayOfWeekOfFirstAug // If the first day of January is any other day, the second Monday is on the 15th
+            }
+            const semStartDate = new Date(currentDate.getFullYear(), august, offset + 7)
+            let weeks = 1
+            let pointer: Date = semStartDate
+
+            while (pointer < currentDate) {
+                weeks += 1
+                pointer.setDate(pointer.getDate() + 7)
+            }
+            weeks -= 1
+            let currentWeek = ""
+            if (weeks <= 7) {
+                currentWeek = `Week ${weeks}`
+            } else if (weeks > 13){
+                currentWeek = "Reading/Exam Week"
+            } else if (weeks > 8) {
+                currentWeek = `Week ${weeks-1}`
+            } else {
+                currentWeek = "Recess Week"
+            }
+            return `AY${currentYear}/${currentYear+1}, Semester 1, ${currentWeek}`
+        }
+    }
 
     return (
         <div className="h-full w-full flex flex-row items-center justify-between">
@@ -31,7 +106,7 @@ export default function NavBar() {
                     ))}
                 </ul>
                 <h1 className="text-sm pl-4 text-slate-800">
-                    AY2023/24, Semester 2, Recess Week
+                    {getSemesterInfo(new Date(2024,3,22))}
                 </h1>
             </div>
         </div>
