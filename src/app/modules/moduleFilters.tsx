19692f21b4465026b83db739b1fcb66df1a361bf
yequan99 <ytan213@e.ntu.edu.sg>
2024-04-14 18:12:54 +0800

    module filter responsive

--- src/app/modules/moduleFilters.tsx
+++ src/app/modules/moduleFilters.tsx
@@ -29,8 +29,8 @@ export default function ModuleFilter({facultyList, filter, setFilter}:{facultyLi
     }
 
     return (
-        <div className="grid grid-cols-6 gap-4 h-10">
-            <div className="col-span-4">
+        <div className="flex flex-col lg:grid lg:grid-cols-6 lg:gap-4 h-10">
+            <div className="lg:col-span-4">
                 <Input 
                     className="w-full h-full"
                     value={filter.query}
@@ -39,7 +39,7 @@ export default function ModuleFilter({facultyList, filter, setFilter}:{facultyLi
                     onChange={(e) => handleQueryChange(e)}
                 />
             </div>
-            <div className="col-span-2">
+            <div className="pt-4 lg:pt-0 lg:col-span-2">
                 <Select
                     className="w-full h-full"
                     defaultValue={filter.faculty}
